fe58259f2077c54251f689bb2d68a1ee789c38e1
Lavish Bana <lavishbana50@gmail.com>
2026-02-25 10:06:31 +0530

    ECE1_JS

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ console.log(typeof(num));
 num = null;
 console.log(num);
 console.log(typeof(num));
+
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if (sym1==sym2){
+    console.log("true");
+}
+else {
+    console.log("false")
+}
