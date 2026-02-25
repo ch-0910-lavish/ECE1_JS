911ffd44aefadd79c3b0a6a1b522706d4cc98d2f
Lavish Bana <lavishbana50@gmail.com>
2026-02-25 09:49:28 +0530

    ECE1_JS

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = "lavish";
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
