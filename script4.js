0bbed57cf2354b5f621e3de76167737f9cb20a96
Lavish Bana <lavishbana50@gmail.com>
2026-03-10 16:31:08 +0530

    script.js

---
+++ script4.js
@@ -0,0 +1,33 @@
+var a=40;
+var b=20;
+var linebreak="<br/>";
+
+console.log("Valueofa=>(a=b)=>");
+result=(a=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("Valueofa=>(a+=b)=>");
+result=(a+=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("Valueofa=>(a-=b)=>");
+result=(a-=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("Valueofa=>(a*=b)=>");
+result=(a*=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("Valueofa=>(a/=b)=>");
+result=(a/=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("Valueofa=>(a%=b)=>");
+result=(a%=b);
+console.log(result);
+console.log(linebreak);
