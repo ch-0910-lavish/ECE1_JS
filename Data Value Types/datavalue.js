4185593b2a04be422f080deaf97b51b70d042daf
Lavish Bana <lavishbana50@gmail.com>
2026-03-11 21:15:33 +0530

    Data Value Types

---
+++ Data Value Types/datavalue.js
@@ -0,0 +1,32 @@
+console.log("Hello World");
+
+var num = 15;
+console.log(num);
+console.log(typeof(num));
+
+num = "lavish";
+console.log(num);
+console.log(typeof(num));
+
+num = false;
+console.log(num);
+console.log(typeof(num));
+
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+num = null;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if (sym1==sym2){
+    console.log("true");
+}
+else {
+    console.log("false")
+}
