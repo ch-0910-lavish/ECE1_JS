8a62b1514ad31f5b3e307ee8fbb2cedba7075b99
Lavish Bana <lavishbana50@gmail.com>
2026-03-11 21:17:53 +0530

    Arithmetic Operator

---
+++ Arithmetic Operator/arithmetic.js
@@ -0,0 +1,27 @@
+
+var a=100; var b=20; var c=10; var linebreak="<br>";
+
+console.log("a+b+c=");
+result=a+b+c;
+console.log(result);
+console.log(linebreak);
+
+console.log("a+b=");
+result=a+b;
+console.log(result);
+console.log(linebreak);
+
+console.log("a-b=");
+result=a-b;
+console.log(result);
+console.log(linebreak);
+
+console.log("a/b=");
+result=a/b;
+console.log(result);
+console.log(linebreak);
+
+console.log("a%b=");
+result=a%b;
+console.log(result);
+console.log(linebreak);
