038d0eed1119bcd5b256527630fd5d0b15516f1f
Lavish Bana <lavishbana50@gmail.com>
2026-03-11 21:21:30 +0530

    Arithmetic Operator

---
+++ Comparison Operator/comparison.js
@@ -0,0 +1,33 @@
+var a=30;
+var b=20;
+var linebreak="<br/>";
+
+console.log("(a==b)=>");
+result=(a==b);
+console.log(result);
+console.log(linebreak);
+
+console.log("(a<b)=>");
+result=(a<b);
+console.log(result);
+console.log(linebreak);
+
+console.log("(a>b)=>");
+result=(a>b);
+console.log(result);
+console.log(linebreak);
+
+console.log("(a!=b)=>");
+result=(a!=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("(a>=b)=>");
+result=(a>=b);
+console.log(result);
+console.log(linebreak);
+
+console.log("(a<=b)=>");
+result=(a<=b);
+console.log(result);
+console.log(linebreak);
