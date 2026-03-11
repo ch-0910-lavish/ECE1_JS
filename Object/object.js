4f6aed757c06e89a82697f2b5daef9a036e033a0
Lavish Bana <lavishbana50@gmail.com>
2026-03-11 21:31:39 +0530

    Object

---
+++ Object/object.js
@@ -0,0 +1,19 @@
+var person={
+    name:"Lavish",
+    age:20, 
+    isMarried:true,
+    address:{
+        street:'Chiranjeev Vihar',
+        flatNO:638
+    }
+};
+console.log(person);
+
+var person2 = new Object();
+person2.name = 'Bobby';
+person2.age = 25;
+person2.isMarried = false;
+person2.address = {}
+person2.address.street = "Gandhi Nagar";
+person2.address.flatNO = 101;
+console.log(person2);
