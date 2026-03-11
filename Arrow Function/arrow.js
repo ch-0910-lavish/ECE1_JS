cbbbecf59dce659c790b6d4bd6df4cc31c8997bf
Lavish Bana <lavishbana50@gmail.com>
2026-03-11 21:23:41 +0530

    Arrow Function

---
+++ Arrow Function/arrow.js
@@ -0,0 +1,12 @@
+//multiple
+let res = mul(12,30);
+function mul(a,b){
+    return a*b;
+}
+console.log("The result of multiplication is: ", res);
+
+// normal function
+let add2 = (x,y) => {
+    console.log("The sum is: ", x+y);
+}
+add2(10,20);
