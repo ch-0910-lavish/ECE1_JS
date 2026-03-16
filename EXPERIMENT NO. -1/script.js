b0ded884d97323bcad7eb17056b59e85e401df1e
Lavish Bana <lavishbana50@gmail.com>
2026-03-16 22:38:30 +0530

    EXPERIMENT NO. -1

---
+++ EXPERIMENT NO. -1/script.js
@@ -0,0 +1,27 @@
+function calculateResult(){
+    let n = document.getElementById("subject").value;
+    let i ;
+    let total=0;
+    for(i=0;i<n;i++){
+        let x = (parseFloat(prompt("Enter the subject No. " + (i+1))));
+        total += x;
+    }
+        let average = total/n;
+        let grade;
+        if(average >= 75){
+            grade = "A";
+        }else if(average >= 60){
+            grade = "B";
+        }else if(average >= 50){
+            grade = "C";
+        }else if(average >= 40){
+            grade = "D";
+        }else{
+            grade = "F";
+        }
+
+    let r = document.getElementById("result").innerHTML = 
+        "Total Marks: " + total.toFixed(1) + "<br>" + 
+        "Average Marks: " + average.toFixed(1) + "<br>" + 
+        `<span class="grade grade-${grade}">Grade: ${grade}</span>`;
+}
